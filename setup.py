3c9b7d2cec06e4811413e7047adfe8fd9604141d
NamPhan <sockyone16899@gmail.com>
2020-08-05 10:44:28 +0700

    upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="wilibs", # Replace with your own username
+    version="0.0.2",
+    author="Revotech",
+    author_email="support@i2g.cloud",
+    description="Wilib",
+    long_description="Wilib",
+    long_description_content_type="text/markdown",
+    url="https://github.com/revotechUET/wilibs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
